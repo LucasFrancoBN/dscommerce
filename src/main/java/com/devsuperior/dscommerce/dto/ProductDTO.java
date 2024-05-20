2f123bfd7f60655345f32ca80bb45ff1bfd7700f
Lucas <lucasfbnavarro@gmail.com>
2024-05-20 20:47:21 -0300

    fix validations

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,6 @@
 package com.devsuperior.dscommerce.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 
@@ -15,6 +12,7 @@ public record ProductDTO(
     @Size(min = 10, message = "Description field must have at least 10 characters")
     @NotBlank(message = "Required field")
     String description,
+    @NotNull(message = "Campo requerido")
     @Positive(message = "Price must be positive value")
     Double price,
     String imgUrl,
